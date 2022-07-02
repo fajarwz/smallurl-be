cbada0f7484e9de2c050809da9c372c261e5cf43
fajarwz <fajarwindhuzulfikar@gmail.com>
2022-07-02 16:49:33 +0700

    code: visit test

---
+++ tests/Unit/Models/VisitTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\ShortUrl;
+use App\Models\Visit;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Illuminate\Support\Collection;
+
+class VisitTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_visit_model_output_date_has_correct_format()
+    {
+        $now = date('Y-m-d H:i:s');
+        $createVisit = Visit::factory()->create();
+
+        $this->assertEquals($createVisit->created_at, $now);
+    }
+
+    /** @test */
+    public function a_visit_has_belongs_to_short_url_relation()
+    {
+        $createVisit = Visit::factory()->create();
+
+        $this->assertInstanceOf(ShortUrl::class, $createVisit->shortUrl);
+        $this->assertEquals($createVisit->short_url_id, $createVisit->shortUrl->id);
+    }
+}
