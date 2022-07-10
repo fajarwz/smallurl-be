94ea5be8461b83257fd6fdafbd229c31c071698d
fajarwz <fajarwindhuzulfikar@gmail.com>
2022-07-10 23:15:32 +0700

    code: register docs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
     /**
      * Login
      * @OA\Post (
-     *     path="/api/login",
+     *     path="/api/v1/login",
      *     tags={"Auth"},
      *     @OA\RequestBody(
      *         @OA\MediaType(
@@ -43,7 +43,7 @@ class AuthController extends Controller
      *      ),
      *      @OA\Response(
      *          response=200,
-     *          description="success",
+     *          description="Valid credentials",
      *          @OA\JsonContent(
      *              @OA\Property(property="meta", type="object",
      *                  @OA\Property(property="code", type="number", example=200),
@@ -68,10 +68,15 @@ class AuthController extends Controller
      *          )
      *      ),
      *      @OA\Response(
-     *          response=400,
-     *          description="invalid",
+     *          response=401,
+     *          description="Invalid credentials",
      *          @OA\JsonContent(
-     *              @OA\Property(property="msg", type="string", example="fail"),
+     *              @OA\Property(property="meta", type="object",
+     *                  @OA\Property(property="code", type="number", example=401),
+     *                  @OA\Property(property="status", type="string", example="error"),
+     *                  @OA\Property(property="message", type="string", example="Incorrect username or password!"),
+     *              ),
+     *              @OA\Property(property="data", type="object", example={}),
      *          )
      *      )
      * )
@@ -96,6 +101,85 @@ class AuthController extends Controller
 
     }
 
+    /**
+     * Register
+     * @OA\Post (
+     *     path="/api/v1/register",
+     *     tags={"Auth"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="name",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="email",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="password",
+     *                          type="string"
+     *                      )
+     *                 ),
+     *                 example={
+     *                     "name":"User",
+     *                     "email":"user@example.com",
+     *                     "password":"user1"
+     *                }
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="meta", type="object",
+     *                  @OA\Property(property="code", type="number", example=200),
+     *                  @OA\Property(property="status", type="string", example="success"),
+     *                  @OA\Property(property="message", type="string", example=null),
+     *              ),
+     *              @OA\Property(property="data", type="object",
+     *                  @OA\Property(property="user", type="object",
+     *                      @OA\Property(property="id", type="number", example=1),
+     *                      @OA\Property(property="name", type="string", example="User"),
+     *                      @OA\Property(property="email", type="string", example="user@example.com"),
+     *                      @OA\Property(property="email_verified_at", type="string", example=null),
+     *                      @OA\Property(property="updated_at", type="string", example="2022-06-28 06:06:17"),
+     *                      @OA\Property(property="created_at", type="string", example="2022-06-28 06:06:17"),
+     *                  ),
+     *                  @OA\Property(property="access_token", type="object",
+     *                      @OA\Property(property="token", type="string", example="randomtokenasfhajskfhajf398rureuuhfdshk"),
+     *                      @OA\Property(property="type", type="string", example="Bearer"),
+     *                      @OA\Property(property="expires_in", type="number", example=3600),
+     *                  ),
+     *              ),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Email already registered",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="meta", type="object",
+     *                  @OA\Property(property="code", type="number", example=422),
+     *                  @OA\Property(property="status", type="string", example="error"),
+     *                  @OA\Property(property="message", type="object",
+     *                      @OA\Property(property="email", type="array", collectionFormat="multi",
+     *                        @OA\Items(
+     *                          type="string",
+     *                          example="The email has already been taken.",
+     *                          )
+     *                      ),
+     *                  ),
+     *              ),
+     *              @OA\Property(property="data", type="object", example={}),
+     *          )
+     *      )
+     * )
+     */
     public function register(RegisterRequest $request)
     {
         $request = $request->validated();
